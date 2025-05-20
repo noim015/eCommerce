2ac02e756600d985525791450b72b1d7fa807fa7
noim015 <sayed.noim.ahmed@gmail.com>
2025-05-20 22:18:52 +1000

    Add order controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,35 @@
+import Order from '../models/Order.js';
+
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
+const addOrderItems = async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (!orderItems || orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order items');
+  }
+
+  const order = new Order({
+    user: req.user._id,
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  });
+
+  const createdOrder = await order.save();
+  res.status(201).json(createdOrder);
+};
+
+export { addOrderItems };
