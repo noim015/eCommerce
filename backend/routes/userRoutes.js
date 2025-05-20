3383c4154120308518dd73730d225fc342b8199b
noim015 <sayed.noim.ahmed@gmail.com>
2025-05-20 21:45:57 +1000

    Add product model

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 const router = express.Router();
-
+import { protect } from '../middleware/authMiddleware.js';
 import { registerUser, loginUser } from '../controllers/userController.js';
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+router.get('/profile', protect, (req, res) => {
+  res.json(req.user);
+});
 
 export default router;
