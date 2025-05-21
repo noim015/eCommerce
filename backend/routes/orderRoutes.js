980ac09eb1fe783082043cfb406c6f022b9b94b3
noim015 <sayed.noim.ahmed@gmail.com>
2025-05-21 10:17:17 +1000

    Update order route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,18 +1,22 @@
 import express from 'express';
 const router = express.Router();
 
-import { protect } from '../middleware/authMiddleware.js';
+import { protect,admin } from '../middleware/authMiddleware.js';
 import {
   addOrderItems,
   getMyOrders,
   getOrderById,
   updateOrderToPaid,
+  getAllOrders,
+  updateOrderToDelivered,
 } from '../controllers/orderController.js';
 
 router.post('/', protect, addOrderItems);
 router.get('/myorders', protect, getMyOrders);
+router.get('/', protect, admin, getAllOrders);
 router.get('/:id', protect, getOrderById);
 router.put('/:id/pay', protect, updateOrderToPaid);
+router.put('/:id/deliver', protect, admin, updateOrderToDelivered);
 
 
 export default router;
